d7c107ee31f3978a756912caa6062877bdf3b40c
eliab <eliab@LAPTOP-JUOF0EID>
2020-11-04 17:59:26 +0100

    class connectToServer created.

--- serverconn.py
+++ serverconn.py
@@ -1,16 +1,19 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Nov  4 16:18:28 2020
+import telnetlib
 
-@author: Diletta
-"""
+class connectToServer(object):
 
-import getpass, sys, telnetlib
-#HOST = "margot.di.unipi.it, 8421"
-tn = telnetlib.Telnet('margot.di.unipi.it', '8421')
+    def __init__(self):
+        print("inside __init__ of Tel class")
+        self.HOST = "margot.di.unipi.it"
+        self.port = "8421"
+        self.net = telnetlib.Telnet(self.HOST, self.port)
+        print("Connected")
 
-tn.write("NEW test28".encode('utf-8')) # pass bytes, not string
-#print(tn.read_all().decode('ascii')) # la read_all blocca tutto 
-tn.read_some()
-#tn.read_until(b"OK", timeout=10) # using b in order to pass bytes avoiding 'encode'
-# test JOIN charlie H - first player
+    def sendcmd(self, command):
+        self.net.write(command.encode('utf-8') + b"\n")
+        response = [text.strip() for text in self.net.read_until(b")", 10).decode('utf-8').splitlines()]
+        return response
+
+ne_obj = connectToServer()
+cmdoutput = ne_obj.sendcmd("test LOOK")
+print(cmdoutput)
