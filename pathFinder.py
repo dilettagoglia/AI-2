1b149ab5c28a35c0552dcb39b2cfdf3951d9152c
Diletta Goglia <diletta.goglia@gmail.com>
2020-11-13 17:31:49 +0100

    It takes in input the parsed map

---
+++ pathFinder.py
@@ -0,0 +1,59 @@
+from pathfinding.core.diagonal_movement import DiagonalMovement
+from pathfinding.core.grid import Grid
+from pathfinding.finder.a_star import AStarFinder
+from pathfinding.finder.breadth_first import BreadthFirstFinder
+# from pathfinding.finder.best_first import BestFirstFinder
+from pathfinding.finder.dijkstra import DijkstraFinder
+from pathfinding.finder.bi_a_star import BiAStarFinder          # the best one
+from pathfinding.finder.ida_star import IDAStarFinder
+
+# Any value smaller or equal to 0 describes an obstacle. Any number bigger than 0 describes the weight of a field
+# that can be walked on. The bigger the number the higher the cost to walk that field.
+# Only Dijkstra and A* take the weight of the fields on the map into account.
+
+# You can use negative values to describe different types of obstacles.
+# It does not make a difference for the path finding algorithm but it might be useful for your later map evaluation.
+
+# Create a map using a 2D-list.
+parsedMap = [
+  [1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0],
+  [1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1],
+  [1,1,1,-6,0,0,0,0,0,0,0,1,1,0,0,0],  # valori negativi vengono considerati sempre ostacoli ma ostacoli di tipo diverso
+  [1,1,1,1,1,-3,1,1,1,1,1,1,0,0,0,0],  # possiamo usarli per distinguere i vari tipi di ostacoli (es. muro/oceano)
+  [1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1],
+  [1,1,1,0,1,1,1,1,1,1,1,1,1,0,0,1],
+  [1,1,1,0,1,1,1,1,1,1,1,1,1,0,0,0],
+  [1,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0],
+  [1,1,0,1,1,1,1,1,1,1,1,1,1,0,0,0],
+  [1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,0],
+  [1,1,0,1,1,1,0,0,0,1,1,1,1,1,1,0],
+  [1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1],
+  [1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1],
+  [1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1],
+  [1,1,1,1,1,1,1,1,1,1,1,1,0,1,0,0],
+  [1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0]
+]
+
+grid = Grid(matrix=parsedMap)
+
+start = grid.node(2, 2)
+end = grid.node(12, 15)
+
+finder = BiAStarFinder(diagonal_movement=DiagonalMovement.never)
+path, runs = finder.find_path(start, end, grid)
+# The find_path function does not only return you the path from the start to the end point it also returns the number
+# of times the algorithm needed to be called until a way was found.
+
+print('operations:', runs, 'path length:', len(path))
+print(grid.grid_str(path=path, start=start, end=end))
+
+# Currently there are 6 path-finders bundled in this library:
+
+#                                     OPERATIONS for path length = 24
+
+# A*                                  108
+# Dijkstra                            196
+# Best-First                          ?
+# Bi-directional A*                   67
+# Breadth First Search (BFS)          196
+# Iterative Deeping A* (IDA*)         661
