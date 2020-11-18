7591a39c9f19f75c587392132ec87e6a77fbce8c
eliab <eliab@LAPTOP-JUOF0EID>
2020-11-18 22:36:51 +0100

    Gestito il Delay.

--- connection/serverConnection.py
+++ connection/serverConnection.py
@@ -1,5 +1,7 @@
 import telnetlib
 
+import time
+
 
 # Driver per la connessione tra Server e il nostro sistema. Resta in esecuzione mantenendo la connessione con il server.
 # Internamente offrirà una socket per leggere e scrivere
@@ -20,26 +22,41 @@ class connectToServer(object):
         """
         self.HOST = host
         self.port = port
-        self.delay = delay
+        self.delay = float(delay)
         try:
             self.net = telnetlib.Telnet(self.HOST, self.port)
+            self.ts = time.time()
         except:
             print("Connection Error")
             return
 
     def send(self, command):
         """
-        Send a string to the server.
+        Send a string to the server. Wait a prefixed delay checking the difference between the last 'send' timestamp with the new one
         :param command: define the command to send to the server.
         :return: the response from server.
         """
-        response = None
+        response = []
+
+        if abs(self.ts - time.time()) < float(self.delay):
+            print( "sleep per : " + str(float(self.delay) - abs(self.ts - time.time())))
+            time.sleep(float(self.delay) - abs(self.ts - time.time()))
+        self.ts = time.time()
+
         self.net.write(command.encode('utf-8') + b"\n")
 
-        while response is None or response is [] or response is "ERROR 401 Too fast":
-            response=[]
-            response = [text.strip() for text in self.net.read_until(b")", float(self.delay)).decode('utf-8').splitlines()]
+        response = [text.strip() for text in self.net.read_until(b"\n").decode('utf-8').splitlines()]
+
+        # if(response[0] == "ERROR 401 Too fast"):
+        #    per ora non usato perchè if True Jarvis ci chiude la socket
+        #    return self.send(command)
+
+        if response[0] == "OK LONG":
+            nextField = ""
+            while nextField != "«ENDOFSTATUS»" and nextField != "«ENDOFMAP»":
+
+                nextField = self.net.read_until(b"\n").decode('utf-8').rstrip()
+                response.append((nextField))
 
 
         return response
-
