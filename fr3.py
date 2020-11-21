7577aa467e35544bbd58ade1120194edb9572bde
eliab <eliab@LAPTOP-JUOF0EID>
2020-11-21 12:59:37 +0100

    inizio di fuzzy

--- fr3.py
+++ fr3.py
@@ -3,79 +3,98 @@ import skfuzzy as fuzz
 from skfuzzy import control as ctrl
 from matplotlib import pyplot as plt
 
-# New Antecedent/Consequent objects hold universe variables and membership
-# functions
-energia = ctrl.Antecedent(np.arange(0, 26, 1), 'energia')
-d_flag = ctrl.Antecedent(np.arange(0, 32, 1), 'd_flag')
+# karen.fuzzyStrategy chiama inputFuzzy
+# creare una funzione inputFuzzy che generi i valori di input per FuzzyControlSystem (calcolando i vari pathfinder verso tutte le possibili destinazioni).
+# Dopo aver chiamato la funzione FuzzyControlSystem, aspetta il return sim.output e in base a questo valore sceglie verso dove muoversi.
+# a questo punto inputFuzzy restituisce a karen.fuzzystrategy le coordinate scelte.
 
-output = ctrl.Consequent(np.arange(0, 20, 1), 'output')
-
-output['ricarica'] = fuzz.trimf(output.universe, [0, 10, 15])
-output['no'] = fuzz.trimf(output.universe, [10, 20, 20])
-
-# Auto-membership function population is possible with .automf(3, 5, or 7)
-energia.automf(3)
-d_flag.automf(3)
+# OTTIMIZZAZIONE: inputFuzzy calcola già tutti i pathfinder, quindi anche quello prescelto. karen.fuzzystrategy passerà ad act già la "direction" e non le coordinate.
 
 
-rule1 = ctrl.Rule((energia['poor'] & d_flag['good']) |
-                  (energia['poor'] & d_flag['average']), output['ricarica'])
-rule2 = ctrl.Rule((energia['poor'] & d_flag['poor']) |
-                  (energia['average'] & d_flag['poor']) |
-                  (energia['good'] & d_flag['poor']) |
-                  (energia['average'] & d_flag['average']) |
-                  (energia['average'] & d_flag['good']) |
-                  (energia['good'] & d_flag['good']), output['no'] )
-system = ctrl.ControlSystem(rules=[rule1, rule2])
+def FuzzyControlSystem():
+    """
+    Output values:
+    -goToRecharge,  0-10
+    -hideBehindTheWall 10-20
+    -useTheBarrier 20-30
+    -goToFlag, 30-40
+    -goToKill, 40-50
 
-# Later we intend to run this system with a 21*21 set of inputs, so we allow
-# that many plus one unique runs before results are flushed.
-# Subsequent runs would return in 1/8 the time!
-sim = ctrl.ControlSystemSimulation(system)
+    """
 
-# Pass inputs to the ControlSystem using Antecedent labels with Pythonic API
-# Note: if you like passing many inputs all at once, use .inputs(dict_of_data)
-sim.input['energia'] = 26
-sim.input['d_flag'] = 1
+    # New Antecedent/Consequent objects hold universe variables and membership functions
 
-# Crunch the numbers
-sim.compute()
+    energy = ctrl.Antecedent(np.arange(0, 256, 10), 'energy')
+    d_flag = ctrl.Antecedent(np.arange(0, 32, 1), 'd_flag')
+    d_recharge = ctrl.Antecedent(np.arange(0, 32, 1), 'd_recharge')
+    num_enemies = ctrl.Antecedent(np.arange(0, 20, 1), 'num_enemies')
+    d_safeZone = ctrl.Antecedent(np.arange(0, 32, 1), 'd_safeZone')
+    d_barrier = ctrl.Antecedent(np.arange(0, 32, 1), 'd_barrier')
+    # d_nearestEnemy = ctrl.Antecedent(np.arange(0, 32, 1), 'd_nearestEnemy')
 
-print (sim.output['output'])
+    output = ctrl.Consequent(np.arange(0, 50, 1), 'output')
+
+    output['goToRecharge'] = fuzz.trimf(output.universe, [0, 10, 10])
+    output['hideBehindTheWall'] = fuzz.trimf(output.universe, [10, 20, 20])
+    output['useTheBarrier'] = fuzz.trimf(output.universe, [20, 30, 30])
+    output['goToFlag'] = fuzz.trimf(output.universe, [30, 40, 40])
+    output['goToKill'] = fuzz.trimf(output.universe, [40, 50, 50])
+
+    # Auto-membership function population is possible with .automf(3, 5, or 7)
+
+    energy.automf(3)
+    d_recharge.automf(3)
+    d_barrier.automf(3)
+    d_flag.automf(3)
+    d_safeZone.automf(3)
+    num_enemies.automf(3)
+
+    # poor mediocre average decent good
+    rule1 = ctrl.Rule((energy['poor'] & d_flag['average'] & (num_enemies['good'] | num_enemies['average'])) |
+                      (energy['poor'] & d_flag['good']) |
+                      (energy['poor'] & d_recharge['poor']) |
+                      (energy['average'] & d_recharge['poor'])
+                      , output['goToRecharge'])
+
+    rule2 = ctrl.Rule((energy['poor'] & d_safeZone['poor']) |
+                      (energy['average'] & (num_enemies['average'] | num_enemies['good']) & d_safeZone['average']) |
+                      (d_safeZone['poor'] & (num_enemies['average'] | num_enemies['good']))
+                      , output['hideBehindTheWall'])
+
+    rule3 = ctrl.Rule(d_barrier['poor'] |
+                      (energy['average'] & (num_enemies['average'] | num_enemies['good']) & (
+                                  d_barrier['poor'] | d_barrier['average']))
+                      , output['useTheBarrier'])
+
+    rule4 = ctrl.Rule(d_flag['poor'] |
+                      ((d_flag['poor'] | d_flag['average']) & num_enemies['poor']) |
+                      num_enemies['poor'] |
+                      (energy['good'] & num_enemies['poor']) & (d_safeZone['good'] | d_safeZone['average'])
+                      , output['goToFlag'])
+
+    rule5 = ctrl.Rule(num_enemies['poor'] & (energy['average'] | energy['good'])|
+                      (num_enemies['good'] & (d_barrier['good']) & d_safeZone['good'])
+                      , output['goToKill'])
+
+    system = ctrl.ControlSystem(rules=[rule1, rule2, rule3, rule4, rule5])
+
+    # Later we intend to run this system with a 21*21 set of inputs, so we almediocre
+    # that many plus one unique runs before results are flushed.
+    # Subsequent runs would return in 1/8 the time!
+    sim = ctrl.ControlSystemSimulation(system)
+
+    # Pass inputs to the ControlSystem using Antecedent labels with Pythonic API
+    # Note: if you like passing many inputs all at once, use .inputs(dict_of_data)
+    sim.input['energy'] = 20
+    sim.input['d_flag'] = 32
+    sim.input['d_recharge'] = 32
+    sim.input['num_enemies'] = 20
+    sim.input['d_safeZone'] = 30
+    sim.input['d_barrier'] = 32
+
+    # Crunch the numbers
+    sim.compute()
+    print(sim.output)
 
 
-
-# We can simulate at higher resolution with full accuracy
-upsampled = np.linspace(0, 26, 21)
-x1, y1 = np.meshgrid(upsampled, upsampled)
-
-upsampled = np.linspace(0, 32, 21)
-x2, y2 = np.meshgrid(upsampled, upsampled)
-z = np.zeros_like(20)
-
-
-# Loop through the system 21*21 times to collect the control surface
-for i in range(21):
-    for j in range(21):
-        sim.input['energia'] = x1[i, j]
-        sim.input['d_flag'] = y2[i, j]
-        sim.compute()
-        z[i, j] = sim.output['output']
-
-
-# Plot the result in pretty 3D with alpha blending
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D  # Required for 3D plotting
-
-fig = plt.figure(figsize=(8, 8))
-ax = fig.add_subplot(111, projection='3d')
-
-surf = ax.plot_surface(x, y, z, rstride=1, cstride=1, cmap='viridis',
-                       linewidth=0.4, antialiased=True)
-
-cset = ax.contourf(x, y, z, zdir='z', offset=-2.5, cmap='viridis', alpha=0.5)
-cset = ax.contourf(x, y, z, zdir='x', offset=3, cmap='viridis', alpha=0.5)
-cset = ax.contourf(x, y, z, zdir='y', offset=3, cmap='viridis', alpha=0.5)
-
-ax.view_init(30, 200)
-plt.show()
+FuzzyStrategy()
